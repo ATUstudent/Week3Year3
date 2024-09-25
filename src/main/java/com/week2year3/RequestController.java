99534f6b7f37a245bad0f5b5558f18bbf9c78bf7
G00422935@atu.ie <g00422935@atu.ie>
2024-09-25 14:57:31 +0100

    Adding GetMapping (hello)

--- src/main/java/com/week2year3/RequestController.java
+++ src/main/java/com/week2year3/RequestController.java
@@ -1,12 +1,30 @@
 package com.week2year3;
 
+import com.fasterxml.jackson.core.sym.Name;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/person")
 public class RequestController {
+@GetMapping("hello")
+    public String message()
+{
+    return "hello";
+}
+@GetMapping("greet")
+public String message2()
+{
+    return "greet";
+}
+@GetMapping("/greet/{name}")
+    public String myname(@PathVariable String Name)
+{
+    return "Your name is " + Name;
+}
+
 
 
 }
