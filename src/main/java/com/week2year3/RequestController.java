955dee17d0b29ca402ac57a08e214c62e65ae157
G00422935@atu.ie <g00422935@atu.ie>
2024-09-25 15:28:34 +0100

    Adding @GetMapping connected to PathVariable

--- src/main/java/com/week2year3/RequestController.java
+++ src/main/java/com/week2year3/RequestController.java
@@ -1,29 +1,32 @@
 package com.week2year3;
 
 import com.fasterxml.jackson.core.sym.Name;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/person")
 public class RequestController {
-@GetMapping("hello")
+@GetMapping("/hello")
     public String message()
 {
     return "hello";
 }
-@GetMapping("greet")
+@GetMapping("/greet")
 public String message2()
 {
     return "greet";
 }
 @GetMapping("/greet/{name}")
-    public String myname(@PathVariable String Name)
+    public String myname(@PathVariable String name)
 {
-    return "Your name is " + Name;
+    return "My name is " + name;
 }
+
+@GetMapping("/details")
+    public String info(@RequestParam String name1, @RequestParam String age)
+    {
+        return"I am " + name1 + "My age is " + age;
+    }
 
 
 
