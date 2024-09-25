3939a6f9f643a46d2d7d80724057a03accef524e
G00422935@atu.ie <G00422935@atu.ie>
2024-09-25 14:52:15 +0100

    starting

---
+++ src/main/java/com/week2year3/Week2Year3Application.java
@@ -0,0 +1,13 @@
+package com.week2year3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2Year3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week2Year3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/week2year3/Week2Year3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.week2year3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2Year3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
