55b2e151059490c16959035783ee78b9e3cc97cb
G00422935@atu.ie <G00422935@atu.ie>
2024-10-01 21:09:02 +0100

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/week2year3/RequestController.java
+++ src/main/java/com/week2year3/RequestController.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.sym.Name;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/person")
+//@RequestMapping("/person")
 public class RequestController {
 @GetMapping("/hello")
     public String message()
@@ -27,9 +27,8 @@ public String message2()
     {
         return"I am " + name1 + "My age is " + age;
     }
-
     @GetMapping("/Calculator")
-    public String calculator(@RequestParam float num1, @RequestParam float num2, String maths)
+    public String calculator(@RequestParam float num1, @RequestParam (defaultValue = "0.0") float num2, String maths)
     {
         float total = 0;
         switch(maths)
@@ -47,10 +46,16 @@ public String message2()
                 break;
 
             case"/":
-                total = num1 / num2;
+                if (num2 != 0) {
+                    total = num1 / num2;
+                } else {
+                    return "Error";
+                }
                 break;
+            default:
+                return "Error: Invalid operation.";
         }
-        return"First Number " + num1 + "Second number " + num2 + "Total is " + total;
+        return "Total is " + total;
     }
 
 }
