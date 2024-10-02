1379949cdb826af017a2fe53630b0707c79e8a10
G00422935@atu.ie <G00422935@atu.ie>
2024-10-02 15:14:30 +0100

    Start

---
+++ Week3Year3/src/main/java/ie/atu/week3year3/ProductController.java
@@ -0,0 +1,39 @@
+package ie.atu.week3year3;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+
+public class ProductController {
+
+    private List<Product> products = new ArrayList<>();
+    public ProductController(){
+        Product a = new Product("Tv", "Made by Sony", 889,100);
+        Product b = new Product("Radio", "Samsung", 99,101);
+    }
+    @GetMapping("/getproducts")
+    public List<Product> getProduct()
+    {
+        return products;
+    }
+    @PostMapping("addProduct")
+    public ResponseEntity addProduct(Product product)
+    {
+        products.add(product);
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/getproducts")
+    public String getProducts()
+    {
+        return "products";
+    }
+}
